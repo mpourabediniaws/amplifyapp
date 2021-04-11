e5021270c044cae364e5dcd8276f6e27dd028f46
mpourabedini <m.pourabedini1991@gmail.com>
2021-04-12 02:09:12 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Amplify from "aws-amplify";
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsExports from "./aws-exports";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+Amplify.configure(awsconfig);
+
+const AuthStateApp: React.FunctionComponent = () => {
+    const [authState, setAuthState] = React.useState<AuthState>();
+    const [user, setUser] = React.useState<object | undefined>();
+
+    React.useEffect(() => {
+        return onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData)
+        });
+    }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+      <div className="App">
+          <div>Hello, {user.username}</div>
+          <AmplifySignOut />
+      </div>
+  ) : (
+      <AmplifyAuthenticator />
   );
 }
 
-export default App;
+export default AuthStateApp;
